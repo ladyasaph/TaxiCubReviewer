eb7e5e1b516b7a167b076dfc0a1578f57885e2f3
nkcay <nanakwame123@gmail.com>
2011-06-27 13:29:52 +0000

    home view added

--- taxi/thetaxi/views.py
+++ taxi/thetaxi/views.py
@@ -1,1 +1,19 @@
 # Create your views here.
+from django.template import Context, loader
+from django.http import HttpResponse, HttpResponseRedirect
+from models import Taxi, Ratings
+from django import forms
+from django.forms import ModelForm
+from django.views.decorators.csrf import csrf_exempt
+
+class SearchForm(forms.Form):
+	search = forms.TextField()
+
+def home(request,):	 
+	if request.method == 'POST':
+		form = SearchForm(request.POST)
+	else:
+		form = SearchForm()
+	
+	
+
