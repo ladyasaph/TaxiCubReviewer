28b6cf5efe65fa86982c868ec87407b6f9a55e25
nelson <nelsonatso@yahoo.com>
2011-06-27 11:31:28 +0000

    committing setting changes

--- taxi/settings.py
+++ taxi/settings.py
@@ -12,7 +12,7 @@ MANAGERS = ADMINS
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.splite3', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': 'taxi.db',                      # Or path to database file if using sqlite3.
+        'NAME': 'thetaxi.db',                      # Or path to database file if using sqlite3.
         'USER': '',                      # Not used with sqlite3.
         'PASSWORD': '',                  # Not used with sqlite3.
         'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
@@ -100,7 +100,7 @@ MIDDLEWARE_CLASSES = (
     'django.contrib.messages.middleware.MessageMiddleware',
 )
 
-ROOT_URLCONF = 'taxi.urls'
+ROOT_URLCONF = 'thetaxi.urls'
 
 TEMPLATE_DIRS = (
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
