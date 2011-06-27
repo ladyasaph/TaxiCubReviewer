3211f0e49530acabce2a81fd8a48c3f229c7f388
nelson <nelsonatso@yahoo.com>
2011-06-27 14:22:36 +0000

    nelson commiting views

--- taxi/thetaxi/views.py
+++ taxi/thetaxi/views.py
@@ -1,1 +1,20 @@
-# Create your views here.
+class TaxiForm(ModelForm):
+	class Meta:
+		model =Ratings
+		exclude=['created']
+	
+
+
+@csrf_exempt
+def add(request):
+	
+	if request.method == 'POST':
+		form = TaxiForm(request.POST)
+		if form.is_valid():
+			form.save()
+			return HttpResponseRedirect(request.path)
+	else:
+		form =TaxiForm()
+	t = loader.get_template('thetaxi/add.html')
+	c = Context({'form':form.as_p()})
+	return HttpResponse(t.render(c))	
